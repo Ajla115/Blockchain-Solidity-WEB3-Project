50eafc4d61bea71750b4ca20ed4eee0201a14396
ilmagusinac <ilma.gusinac@stu.ibu.edu.ba>
2023-12-28 12:39:58 +0100

    adding frontend template

--- contracts/NewsPortal.sol
+++ contracts/NewsPortal.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.22;
+pragma solidity 0.8.19;
 
 //NAPOMENA: Razdvojiti  komentare po postu (valjda smo to uradile)
 //NAPOMENA: //MOZDA MOZE OVAKO KAKO SAM KRENULA za positions
@@ -429,6 +429,7 @@ contract NewsPortal {
     }
 
 }
+
 
     
 
