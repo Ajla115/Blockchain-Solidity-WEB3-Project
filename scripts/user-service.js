afb48ef7836d9d8e228b5eff19076fd60188ac70
ilmagusinac <ilma.gusinac@stu.ibu.edu.ba>
2024-01-07 20:36:23 +0100

    added changes to indexajla2

--- scripts/user-service.js
+++ scripts/user-service.js
@@ -2,14 +2,7 @@ var UserService = {
    
     logout: function () {
       localStorage.clear();
-      // Hide user-related sections
-      $('#userDiv').hide();
-      $('#commentDisplay').hide();
-      $('#commentFormModal').hide();
-      $('#commentsDisplay').hide();
-
-      // Show the login section
-      $('#LoginDiv').show();
+      window.location.replace("indexajla2.html");
     }
   };
   
