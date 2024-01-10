538e7dd4897e7fcce9b6ae02ef2e65cc58cc32da
Ajla Korman <ajla.korman@stu.ibu.edu.ba>
2024-01-10 19:36:53 +0100

    Modified backend, added necessary features to frontend, and wrote js for their correct functionality

--- contracts/NewsPortal.sol
+++ contracts/NewsPortal.sol
@@ -8,44 +8,31 @@ contract NewsPortal {
         string genre;
         string title;
         string content;
-        uint commentsPerPostID;
     }
 
-    struct Comment {
+    struct HelpDeskItem {
         uint id;
-        uint postID;
-        address userWriter;
+        string topic;
         string content;
-        uint numberOfCharacters; //this is the length of the comment
-
     }
 
-    //address private writer; //this is the one and only admin
+    uint private id = 1;                     //ID value for posts, we started with one because it feels more natural
+    uint private helpDeskItemID = 1;         //ID value for help desk items, we started with one because it feels more natural
+    uint private postCounter = 0;            //counter for number of posts
+
+    mapping(uint => Post) allPosts;          // used to store all posts, key: postID, value: Post struct
+    mapping(string => Post[]) postPerGenre;  //used to store posts per genre, key:genre, value:arrays of post structs
     mapping(address => bool) private admins; //we have two admins
 
-    address private usersAddress; //used to save users address from mapping, into a variable, so that user can be also deleted from users array
-
-    uint private id = 1; //ID, that will be used to count number of posts and increment for each new post, we started with 1, because that is more natural
-    uint private postCounter = 0; //this counts the number of posts
-    uint private commentsPerPostID = 0; //number of comments per post
-    uint private userID = 1; //counter to count the number of users, we started with 1, because that is more natural
-
-    mapping(uint => Post) allPosts; // used to store all posts, key: postID, value: Post struct
-    mapping(string => Post[]) postPerGenre; //used to store posts per genre, key:genre, value:arrays of post structs
-    mapping(uint => Comment[]) commentsPerPost; //key:post ID, value:array of comment structs
-    mapping(uint => uint) commentsCountPerPost;
-    mapping(uint => mapping(address => bool)) commentWritersPerPostIDMapping;  //key: post ID, value: another mapping(user's address => T/F), depending if they commented or not
-
-    Post[] posts;  //list of all posts 
-    Comment[] comments; //list of all comments 
-
-    //address[] commentWritersPerPostArray; 
+    Post[] posts;                            //array of all post struct items
+    HelpDeskItem[] helpDeskService;          //array of all help desk struct items
 
     event newPostCreatedEvent(uint id, string genre, string title, string content);
-    event commentWrittenEvent(uint postID, uint commentID);
+    event helpDeskItemWrittenEvent(uint helpDeskItemID, string topic, string content);
     event PostDeletedEvent(uint id);
+    event newAdminAddedEvent(address _newAdminAddress);
     
-    //modifiers to increment and decrement number of posts, and to increment number of comments
+    //modifiers to increment and decrement post counter; increment post and help desk item ID
 
     modifier incrementPostCounter(){  
         _;
@@ -57,83 +44,74 @@ contract NewsPortal {
         id += 1;
     }
 
-
-
     modifier decrementPostCounter(){
         _;
         postCounter -= 1;
     }
 
-    
-    modifier incrementCommentCounter(){
+        modifier incrementhelpDeskItemID(){
         _;
-        commentsPerPostID += 1;
+        helpDeskItemID += 1;
     }
 
-    //access control modifiers, some actions are allowed only to admin, and some are prohibited to admin
-
-
-
+    //modifiers to limit access control, one for admin, and one for user
     modifier onlyUser(){
         require( admins[msg.sender] == false, "You have to be logged in as a user for this.");
         _;
     }
 
     modifier onlyAdmin() {
-    require(admins[msg.sender], "Only an admin can perform this action.");
-    _;
-}
-
-    //modifier to check comment character limit
-
-    modifier checkCommentLength(string memory comment) {
-        require(bytes(comment).length <= 250, "You went over character limit of 250!");
+        require(admins[msg.sender], "Only an admin can perform this action.");
         _;
     }
 
-    //modifier to restrict users to write only one comment per post
-
-    modifier spamControl(uint _postID) {
-        require(!commentWritersPerPostIDMapping[_postID][msg.sender], "You can write only one comment per post.");
+    //modifier to check  character limit
+    modifier checkLength(string memory textToBeTested) {
+        require(bytes(textToBeTested).length <= 250, "You went over character limit of 250!");
         _;
     }
 
     constructor() {
-    admins[msg.sender] = true; // Set the contract deployer as an admin
-}
+        admins[msg.sender] = true; 
+    }
 
-    //function to write posts by an admin
+    //Function to add another admin
+    function addAdmin(address newAdmin) external onlyAdmin {
+        admins[newAdmin] = true;
+        emit newAdminAddedEvent(newAdmin); 
+    }
+
+    //Function to write posts by an admin
     function writePosts(string memory _genre, string memory _title, string memory _content) external incrementPostCounter incrementPostID onlyAdmin {
 
-        
         // Create a new Post 
-        Post memory newPost = Post(id, _genre, _title, _content, 0);
+        Post memory newPost = Post(id, _genre, _title, _content);
         posts.push(newPost);
 
-        // we also need to add new post to both mappings that are reated to post structs
+        //We also need to add new post to both mappings that are related to post structs
         postPerGenre[_genre].push(newPost);
         allPosts[id] = newPost;
 
-        //emit an event that the new post is created
+        //Emit an event that the new post is created
         emit newPostCreatedEvent(id, _genre, _title, _content);
     }
 
-    //function to delete posts by an admin
-    //posts need to be deleted in 3 seperate places, two mappings and one array
+    //Function to delete posts by an admin
+    //Posts need to be deleted in 3 seperate places, two mappings and one array
     function deletePost(uint _id) external onlyAdmin decrementPostCounter {
         string memory extractGenre = allPosts[_id].genre;
 
-        // first, we found index of the post to delete
+        //First, we found index of the post to delete
         uint indexToDelete = findPostIndex(postPerGenre[extractGenre], _id);
 
-        // If the post is in right array, by guessing the right genre, then delete it
+        //If the post is in right array, by finding  the right genre, then delete it
         if (indexToDelete < postPerGenre[extractGenre].length) {
-            // we also swapped positions of the last item in the array and the one we want to delete
+            //We also swapped positions of the last item in the array and the one we want to delete
             postPerGenre[extractGenre][indexToDelete] = postPerGenre[extractGenre][postPerGenre[extractGenre].length - 1];
             postPerGenre[extractGenre].pop();
         }
 
-        //It also needs to be deleted from posts, array of structs
+        //Post needs to be deleted as well  from posts, which is an array of structs
         //Same procedure as above will be followed
         uint postsIndexToDelete = findPostIndex(posts, _id);
 
@@ -142,91 +120,55 @@ contract NewsPortal {
             posts.pop();
         }
 
-        // also delete the post from other mapping as well
+        //Also delete the post from other mapping
         delete allPosts[_id];
         emit PostDeletedEvent(_id);
 
         
     }
 
-    // This will return index of the post in the array
+    //This will return index of the post in the array
+    //Needed as an internal function to delete the post from the posts array
     function findPostIndex(Post[] storage postsArray, uint postId) internal view returns (uint) {
          for (uint i = 0; i < postsArray.length; i++) {
             if (postsArray[i].id == postId) {
                 return i;
             }
         }
-        // it will return array if there is no match
+        //It will return array if there is no match
         return postsArray.length; 
     }
 
-    //function to get current number of posts
-    function getCurrentPostCount() external view returns (uint) {
+    //Function to get current number of posts
+    //Also, it will be called fter event to write and delete posts
+    function getCurrentPostCount() external onlyAdmin view returns (uint) {
         return postCounter;
     }
 
-    function writeComments(uint _postID, string memory _content) external onlyUser incrementCommentCounter checkCommentLength(_content) spamControl(_postID) returns (uint, address, string memory, uint) {
-        uint _numberOfCharacters = bytes(_content).length;
-        Comment memory newComment = Comment(commentsPerPostID, _postID,  msg.sender, _content, _numberOfCharacters);
-        comments.push(newComment);
-    
-        // Update the mapping commentsPerPost
-        commentsPerPost[_postID].push(newComment);
-
-        // Update the mapping to mark the user as a comment writer for this post
-        commentWritersPerPostIDMapping[_postID][msg.sender] = true;
-
-        // Update the comment count for the post
-        commentsCountPerPost[_postID]++;
-
-        // Update the comment count in the Post struct
-        allPosts[_postID].commentsPerPostID = commentsCountPerPost[_postID];
-
-        // Update the comment count for the post per genre
-        string memory genre = allPosts[_postID].genre;
-        uint numberOfCommentsPerPost = commentsCountPerPost[_postID];
-
-        // Update the post in the posts array
-       updatePostCommentCount(posts, _postID, numberOfCommentsPerPost);
-
-        // Update the post in the postPerGenre mapping
-        updatePostCommentCount(postPerGenre[genre], _postID, numberOfCommentsPerPost);
-
-        emit commentWrittenEvent(_postID, newComment.id);
-
-        return (newComment.id, newComment.userWriter, newComment.content, newComment.numberOfCharacters);
-    }
-
-
-    function updatePostCommentCount(Post[] storage postsArray, uint _postID, uint count) internal {
-        uint postIndexToUpdate = findPostIndex(postsArray, _postID);
-            if (postIndexToUpdate < postsArray.length) {
-                postsArray[postIndexToUpdate].commentsPerPostID = count;
-            }
-    }
-
-    //function to get number of comments per post
-    function getCommentCountPerPost(uint _postID) external view returns (uint) {
-        return allPosts[_postID].commentsPerPostID;
-    }
-
-    //function to view all posts (both by admin and users --> joint function)
-    //needed for the event newPostCreatedEvent
-    function getAllPosts() external view returns(Post[] memory){
+    //Function to view all posts
+    //Needed for the event newPostCreatedEvent
+    function getAllPosts() external onlyAdmin view returns(Post[] memory){
         return posts;
     }
 
-    //function to view all posts based on genre
-    //this one will be called after updating genre, but also can be called after updating title and/or content, we will see
+    //Function to write help desk item by a user
+    function writeHelpDeskItem(string memory _topic, string memory _content) external  incrementhelpDeskItemID  checkLength(_content) onlyUser {
+
+        // Create a new help desk item 
+        HelpDeskItem memory newItem = HelpDeskItem(helpDeskItemID, _topic, _content);
+        helpDeskService.push(newItem);
+
+        //Emit an event that the new post is created
+        emit helpDeskItemWrittenEvent(helpDeskItemID, _topic, _content);
+    }
+
+    //Function to view all posts based on genre
+    //This one will be called after updating genre
     function getAllPostsPerGenre(string memory _wantedGenre) external view returns(Post[] memory){
         return postPerGenre[_wantedGenre];
     }
 
-    //function to get all comments per post
-    //needed for event commentWrittenEvent
-    function getCommentsPerPost(uint _postID) external view returns (Comment[] memory){
-        return commentsPerPost[_postID];
-    }
+
 
    
     
